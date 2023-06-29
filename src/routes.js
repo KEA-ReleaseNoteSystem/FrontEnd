856b0dd5c00dce4929d6c37e695529e4d26761b8
manips <jwjw6410@gmail.com>
2023-06-29 16:19:44 +0900

    my-team 수정

--- src/routes.js
+++ src/routes.js
@@ -38,6 +38,7 @@ Coded by www.creative-tim.com
 // Material Dashboard 2 React layouts
 import Dashboard from "layouts/dashboard";
 import Tables from "layouts/tables";
+import Myteam from "layouts/myteam";
 import Billing from "layouts/billing";
 import RTL from "layouts/rtl";
 import Notifications from "layouts/notifications";
@@ -64,6 +65,14 @@ const routes = [
     icon: <Icon fontSize="small">table_view</Icon>,
     route: "/tables",
     component: <Tables />,
+  },
+  {
+    type: "collapse",
+    name: "myteam",
+    key: "myteam",
+    icon: <Icon fontSize="small">table_view</Icon>,
+    route: "/myteam",
+    component: <Myteam />,
   },
   {
     type: "collapse",
