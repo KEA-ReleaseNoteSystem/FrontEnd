94e7c41790bdc63b73518b1ae098f657c3112a31
manips <jwjw6410@gmail.com>
2023-07-05 17:33:45 +0900

    [feat]gpt추천 페이지

--- src/layouts/dashboard/components/Projects/data/index.js
+++ src/layouts/dashboard/components/Projects/data/index.js
@@ -16,38 +16,13 @@ Coded by www.creative-tim.com
 */
 
 // @mui material components
-import Tooltip from "@mui/material/Tooltip";
 import MDBox from "components/MDBox";
 import MDTypography from "components/MDTypography";
-import MDAvatar from "components/MDAvatar";
 import MDProgress from "components/MDProgress";
+import Modal from "@mui/material/Modal";
+import Box from "@mui/material/Box";
 
-// Images
-// 로고 넣기
-import logoXD from "assets/images/small-logos/logo-xd.svg";
-
-// 팀원 넣기
-import team1 from "assets/images/team-1.jpg";
-import team2 from "assets/images/team-2.jpg";
-import team3 from "assets/images/team-3.jpg";
-import team4 from "assets/images/team-4.jpg";
-
-const issues = [
-  {
-    name: "#00 Test issue",
-    member: "서강덕",
-    due: "0000-00-00",
-    value: 30,
-  },
-  {
-    name: "#00 Test issue",
-    member: "서지원",
-    due: "0000-00-00",
-    value: 99,
-  },
-];
-
-export default function data() {
+export default function data({issues}) {
   const Issue = ({ name }) => (
     <MDBox display="flex" alignItems="center" lineHeight={1}>
       <MDTypography variant="button" fontWeight="medium" ml={1} lineHeight={1}>
@@ -63,6 +38,24 @@ export default function data() {
     { Header: "중요도", accessor: "importance", align: "center" },
   ];
 
+
+  const renderProgress = (value) => {
+    if (value === null) {
+      return <MDTypography variant="button" fontWeight="medium" ml={1} lineHeight={1}>지정되지 않음</MDTypography>;
+    }
+
+    const color =
+      value < 30 ? 'info' : value < 60 ? 'warning' : value < 80 ? 'error' : 'primary';
+
+    return (
+      <MDProgress
+        value={value}
+        color={color}
+        variant="gradient"
+        label={false}
+      />
+    );
+  };
   const rows = issues.map((issues) => ({
     issue: <Issue name={issues.name} />,
     member: (
@@ -75,16 +68,15 @@ export default function data() {
         {issues.due}
       </MDTypography>
     ),
-    importance: (
+    importance: (      
       <MDBox width="8rem" textAlign="left">
-        <MDProgress 
-          value={issues.value}
-          color={issues.value<30?"info":issues.value<60?"warning":issues.value<80?"error":"primary"} variant="gradient" label={false} />
+        {renderProgress(issues.value)}
       </MDBox>
     ),
   }));
 
   return {
+    
     columns,
     rows,
   };
