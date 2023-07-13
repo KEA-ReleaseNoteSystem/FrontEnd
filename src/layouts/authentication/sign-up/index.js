6a0783a7117a86236d95c1c8cae496de114cba1e
“kakao99” <rkdejr2321@naver.com>
2023-07-13 14:41:56 +0900

    [fix] 일반 회원가입 할 때 email 유효성 체크

--- src/layouts/authentication/sign-up/index.js
+++ src/layouts/authentication/sign-up/index.js
@@ -78,7 +78,7 @@ function Cover() {
   const isNameEmpty = name === "";
   const isNicknameEmpty = nickname === "";
   const isPositionEmpty = position === "";
-  const isEmailEmpty = (email === "") || !(email.includes("@"));
+  const isEmailEmpty = (email === "") || !(email && email.includes("@"));
   const isPasswordEmpty = password === "";
 
   return (
