4fb00f2b623fd6d002ca5a227ea3a27198a4e46e
Haebin Ahn <gnh06280@naver.com>
2023-07-24 10:13:25 +0900

    [feat] issueManagement 페이지 모달 창 수정/삭제 버튼 누르면 모달 닫기

--- src/layouts/issue-manage/Lists/detail-issue/index.js
+++ src/layouts/issue-manage/Lists/detail-issue/index.js
@@ -13,20 +13,13 @@ Coded by www.creative-tim.com
 * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 */
 import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
 
 // @mui material components
 import Grid from "@mui/material/Grid";
-import Card from "@mui/material/Card";
-import Divider from "@mui/material/Divider";
-import { Icon, IconButton, Menu, MenuItem } from "@mui/material";
-
-// @mui icons
-import FacebookIcon from "@mui/icons-material/Facebook";
-import TwitterIcon from "@mui/icons-material/Twitter";
-import InstagramIcon from "@mui/icons-material/Instagram";
 
 // Material Dashboard 2 React components
 import MDBox from "components/MDBox";
@@ -34,39 +27,11 @@ import MDTypography from "components/MDTypography";
 import MDInput from "components/MDInput";
 import MDButton from "components/MDButton";
 
-// Material Dashboard 2 React example components
-import DashboardLayout from "examples/LayoutContainers/DashboardLayout";
-import DashboardNavbar from "examples/Navbars/DashboardNavbar";
-import Footer from "examples/Footer";
-import ProfileInfoCard from "examples/Cards/InfoCards/ProfileInfoCard";
-import ProfilesList from "examples/Lists/ProfilesList";
-import DefaultProjectCard from "./components/MemberCards"
-
-// Overview page components
-import Header from "layouts/pm/components/Header";
-import PlatformSettings from "layouts/pm/components/PlatformSettings";
-
-// Data
-import profilesListData from "layouts/profile/data/profilesListData";
-
-// Images
-import homeDecor1 from "assets/images/home-decor-1.jpg";
-import homeDecor2 from "assets/images/home-decor-2.jpg";
-import homeDecor3 from "assets/images/home-decor-3.jpg";
-import homeDecor4 from "assets/images/home-decor-4.jpeg";
-import team1 from "assets/images/team-1.jpg";
-import team2 from "assets/images/team-2.jpg";
-import team3 from "assets/images/team-3.jpg";
-import team4 from "assets/images/team-4.jpg";
-
-import CalendarTodayIcon from '@mui/icons-material/CalendarToday';
-import LinearScaleIcon from '@mui/icons-material/LinearScale';
-
 
 
 
 function Overview(issue) {
-
+  const navigate = useNavigate();
   const token = localStorage.getItem('ACCESS_TOKEN');
 
   useEffect(() => {
@@ -87,6 +52,8 @@ function Overview(issue) {
           Authorization: `Bearer ${token}`
       }
   });
+
+  navigate(0);
   }
 
   return (
