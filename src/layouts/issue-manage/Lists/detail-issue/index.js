286d639a9c92bd211a010ceed556624a033c1ff3
Doyoung <doodoo55@gachon.ac.kr>
2023-07-24 10:10:49 +0900

    [feat] 이슈 디테일에서 이슈 삭제 버튼 추가

--- src/layouts/issue-manage/Lists/detail-issue/index.js
+++ src/layouts/issue-manage/Lists/detail-issue/index.js
@@ -63,11 +63,11 @@ import CalendarTodayIcon from '@mui/icons-material/CalendarToday';
 import LinearScaleIcon from '@mui/icons-material/LinearScale';
 
 
+
+
 function Overview(issue) {
-  const defaultValue = '뇌파를 이용한 설문조사 서비스';
-  const [selectedDate, setSelectedDate] = useState(new Date());
-  const [projects, setProjects] = useState([]);
-  const [issueType, setIssueType] = useState(""); // 선택된 이슈 타입을 저장하는 상태
+
+  const token = localStorage.getItem('ACCESS_TOKEN');
 
   useEffect(() => {
     axios.get('/api/projects') // API 엔드포인트를 적절히 변경해야 합니다.
@@ -79,58 +79,73 @@ function Overview(issue) {
       });
   }, []);
 
+  
+
+  const handleOnClickDeleteIssue = async () => {
+    await axios.delete(`api/issue/${issue.issue.id}`,{
+      headers: {
+          Authorization: `Bearer ${token}`
+      }
+  });
+  }
+
   return (
     <MDBox pb={3}>
       <Grid>
         <Grid item xs={12}>
-            <MDBox
-              mx={2}
-              mt={-3}
-              py={3}
-              px={2}
-              variant="gradient"
-              bgColor="info"
-              borderRadius="lg"
-              coloredShadow="info"
-            >
-              <MDTypography variant="h6" color="white">
-                Issue #{issue.issue.id}
-              </MDTypography>
+          <MDBox
+            mx={2}
+            mt={-3}
+            py={3}
+            px={2}
+            variant="gradient"
+            bgColor="info"
+            borderRadius="lg"
+            coloredShadow="info"
+          >
+            <MDTypography variant="h6" color="white">
+              Issue #{issue.issue.id}
+            </MDTypography>
+          </MDBox>
+          <MDBox component="form" role="form" mt={6} ml={3} mr={10}>
+            <MDBox mb={2}>
+              <MDInput type="text" label="제목" defaultValue={issue.issue.title} disabled fullWidth />
             </MDBox>
-            <MDBox component="form" role="form" mt={6} ml={3} mr={10}>
-              <MDBox mb={2}>
-                <MDInput type="text" label="제목" defaultValue={issue.issue.title} disabled fullWidth />
-              </MDBox>
-              <MDBox mb={2}>
-                <MDInput type="text" label="보고자" defaultValue="서강덕" disabled fullWidth />
-              </MDBox>
-              <MDBox mb={2}>
-                <MDInput type="text" label="담당자" defaultValue="안해빈" disabled fullWidth />
-              </MDBox>
-              <MDBox mb={2}>
-                <MDInput
-                  label="타입"
-                  value={issue.issue.type}
-                  disabled
-                />
-                <MDInput
-                  label="상태"
-                  value={issue.issue.status}
-                  disabled
-                />
-              </MDBox>
-              <MDBox mb={2}>
-                <MDInput
-                  label="생성일"
-                  disabled
-                  defaultValue={issue.issue.updatedAt}
+            <MDBox mb={2}>
+              <MDInput type="text" label="보고자" defaultValue="서강덕" disabled fullWidth />
+            </MDBox>
+            <MDBox mb={2}>
+              <MDInput type="text" label="담당자" defaultValue="안해빈" disabled fullWidth />
+            </MDBox>
+            <MDBox mb={2}>
+              <MDInput
+                label="타입"
+                value={issue.issue.type}
+                disabled
+              />
+              <MDInput
+                label="상태"
+                value={issue.issue.status}
+                disabled
+              />
+            </MDBox>
+            <MDBox mb={2}>
+              <MDInput
+                label="생성일"
+                disabled
+                defaultValue={issue.issue.updatedAt}
 
-                />
-              </MDBox>
-              <MDBox mb={2}>
-                <MDInput type="textarea" label="설명" defaultValue={issue.issue.description} disabled rows={4} multiline fullWidth />
-              </MDBox>
+              />
             </MDBox>
+            <MDBox mb={2}>
+              <MDInput type="textarea" label="설명" defaultValue={issue.issue.description} disabled rows={4} multiline fullWidth />
+            </MDBox>
+          </MDBox>
+          <MDBox mt={4} mb={1} display="flex" justifyContent="center">
+            <MDButton variant="gradient" color="info" onClick={handleOnClickDeleteIssue}>
+              이슈 삭제
+            </MDButton>
+          </MDBox>
         </Grid>
       </Grid>
     </MDBox>
