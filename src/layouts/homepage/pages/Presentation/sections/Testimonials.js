07e9a5f5e5fee3afb72ce85b6b39d351f0f2881f
manips <jwjw6410@gmail.com>
2023-07-26 15:10:53 +0900

    [chore] facebook 이미지 오류

--- src/layouts/homepage/pages/Presentation/sections/Testimonials.js
+++ src/layouts/homepage/pages/Presentation/sections/Testimonials.js
@@ -11,7 +11,7 @@ Coded by www.creative-tim.com
  =========================================================
 
 * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-*/
+
 
 // @mui material components
 import Container from "@mui/material/Container";
@@ -110,3 +110,4 @@ function Information() {
 }
 
 export default Information;
+*/
