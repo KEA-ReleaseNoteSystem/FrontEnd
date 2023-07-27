f9e3df1fccea14b91b11c54417c689a70020f009
Doyoung <doodoo55@gachon.ac.kr>
2023-07-27 12:15:44 +0900

    [feat] 홈화면 UI 디자인 완료

--- src/layouts/homepage/pages/Presentation/sections/Pages.js
+++ src/layouts/homepage/pages/Presentation/sections/Pages.js
@@ -43,46 +43,19 @@ function Pages() {
   return (
     <MKBox component="section" py={6}>
       <Container>
-        <Grid
-          container
-          item
-          xs={12}
-          lg={6}
-          flexDirection="column"
-          alignItems="center"
-          sx={{ textAlign: "center", my: 6, mx: "auto", px: 0.75 }}
-        >
-          <MKBadge
-            variant="contained"
-            color="info"
-            badgeContent="boost creativity"
-            container
-            sx={{ mb: 2 }}
-          />
-          <MKTypography variant="h2" fontWeight="bold">
-            With our coded pages
-          </MKTypography>
-          <MKTypography variant="body1" color="text">
-            The easiest way to get started is to use one of our
-            <br /> pre-built example pages.
-          </MKTypography>
-        </Grid>
-      </Container>
-      <Container sx={{ mt: { xs: 8, lg: 16 } }}>
         <Grid container spacing={3}>
           <Grid item xs={12} lg={9} sx={{ mt: 3, px: { xs: 0, lg: 8 } }}>
-            <Grid container spacing={3}>
+            <Grid container spacing={3} >
               {renderData}
             </Grid>
           </Grid>
           <Grid item xs={12} lg={3}>
             <MKBox position="sticky" top="100px" pb={{ xs: 2, lg: 6 }}>
               <MKTypography variant="h3" fontWeight="bold" mb={1}>
-                Presentation Pages for Company, Landing Pages, Blogs and Support
+                Releasy 미리보기
               </MKTypography>
               <MKTypography variant="body2" fontWeight="regular" color="secondary" mb={1} pr={2}>
-                These is just a small selection of the multiple possibitilies you have. Focus on the
-                business, not on the design.
+                편리한 서비스 이용을 위해 직관적인<br/>사용자 경험을 제공합니다.
               </MKTypography>
             </MKBox>
           </Grid>
