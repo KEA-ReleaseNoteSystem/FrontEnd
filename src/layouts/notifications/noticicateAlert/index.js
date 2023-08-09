db0d9982a620cd57150a839b4645785a91c92afd
jedo0224 <qkrwotjr0@gmail.com>
2023-08-09 15:16:05 +0900

    [fix] 이슈 상태 해결 및 발행 시 스낵바 알림 기능 코드 변경

--- src/layouts/notifications/noticicateAlert/index.js
+++ src/layouts/notifications/noticicateAlert/index.js
@@ -1,137 +1,88 @@
-/**
-=========================================================
-* Material Dashboard 2 React - v2.2.0
-=========================================================
-
-* Product Page: https://www.creative-tim.com/product/material-dashboard-react
-* Copyright 2023 Creative Tim (https://www.creative-tim.com)
-
-Coded by www.creative-tim.com
-
- =========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-*/
-
 import { useEffect, useState } from "react";
-
-
 import MDSnackbar from "components/MDSnackbar";
-import { RecoilRoot ,useRecoilState} from "recoil";
-import { projectIdState } from 'examples/Sidenav/ProjectIdAtom';
 
-
-function Notifications({message , projectId}) {
-  console.log("messagemessage",message);
+function Notifications({ message, projectId }) {
   const [successSB, setSuccessSB] = useState(false);
   const [infoSB, setInfoSB] = useState(false);
   const [warningSB, setWarningSB] = useState(false);
   const [errorSB, setErrorSB] = useState(false);
-  const [notification, setNotification] = useState();
- 
 
+  useEffect(() => {
 
-  const openSuccessSB = () => setSuccessSB(true);
-  const closeSuccessSB = () => setSuccessSB(false);
-  const openInfoSB = () => setInfoSB(true);
-  const closeInfoSB = () => setInfoSB(false);
-  const openWarningSB = () => setWarningSB(true);
-  const closeWarningSB = () => setWarningSB(false);
-  const openErrorSB = () => setErrorSB(true);
-  const closeErrorSB = () => setErrorSB(false);
-  // console.log("projectId",projectId);
-
-  useEffect(() =>
-   {
-    
-    if(message && message.message && (projectId == message.projectId)){
-    console.log("message.projectId",message.projectId);
-    setNotification(message.message);
-    }else{
-      setNotification();
+    if (message && message.projectId === projectId) {
+      switch (message.notiType) {
+        case "ISSUEDONE":
+          setSuccessSB(true);
+          break;
+        case "ISSUECREATED": 
+          setInfoSB(true);
+          break;
+        case "ISSUEALLOCATED":
+          setWarningSB(true);
+        case "ISSUEDELETED":
+          setErrorSB(true);
+        default:
+          break;
+      }
     }
+  }, [message, projectId]);
 
-   },[message]);
-
-   useEffect(() =>
-   {
-    console.log("notification",notification);
-    if(notification && notification.includes("해결") ){
-      openSuccessSB();
-    }
-    else if(notification &&  notification.includes("발행") ){
-      openInfoSB();
-    }
-    else{
-      closeSuccessSB();
-      closeInfoSB();
-    }
-
-   },[message]);
-
-
-
-  const renderSuccessSB = (
-    <MDSnackbar
-      color="success"
-      icon="check"
-      title="이슈 해결"
-      content={notification}
-      // dateTime="11 mins ago"
-      open={successSB}
-      onClose={closeSuccessSB}
-      close={closeSuccessSB}
-      bgWhite
-    />
-  );
-
-  const renderInfoSB = (
-    <MDSnackbar
-      icon="notifications"
-      title="이슈 발행"
-      content={notification}
-      // dateTime="11 mins ago"
-      open={infoSB}
-      onClose={closeInfoSB}
-      close={closeInfoSB}
-    />
-  );
-
-  const renderWarningSB = (
-    <MDSnackbar
-      color="warning"
-      icon="star"
-      title="Material Dashboard"
-      content={notification}
-      dateTime="11 mins ago"
-      open={warningSB}
-      onClose={closeWarningSB}
-      close={closeWarningSB}
-      bgWhite
-    />
-  );
-
-  const renderErrorSB = (
-    <MDSnackbar
-      color="error"
-      icon="warning"
-      title="Material Dashboard"
-      content={notification}
-      dateTime="11 mins ago"
-      open={errorSB}
-      onClose={closeErrorSB}
-      close={closeErrorSB}
-      bgWhite
-    />
-  );
+  const closeAllSnackbars = () => {
+    setSuccessSB(false);
+    setInfoSB(false);
+    setWarningSB(false);
+    setErrorSB(false);
+  };
 
   return (
-    <div>
-    {notification !== undefined && renderSuccessSB}
-    {notification !== undefined && renderInfoSB}
-    {notification !== undefined && renderWarningSB}
-    {notification !== undefined && renderErrorSB}
-    </div>
+    <>
+      {successSB && (
+        <MDSnackbar
+          color="success"
+          icon="check"
+          title="이슈 해결"
+          content={message.message}
+          open={successSB}
+          onClose={closeAllSnackbars}
+          close={closeAllSnackbars}
+          bgWhite
+        />
+      )}
+      {infoSB && (
+        <MDSnackbar
+          icon="notifications"
+          title="이슈 발행"
+          content={message.message}
+          open={infoSB}
+          onClose={closeAllSnackbars}
+          close={closeAllSnackbars}
+        />
+      )}
+      {warningSB && (
+        <MDSnackbar
+          color="warning"
+          icon="star"
+          title="이슈 변경"
+          content={message.message}
+          open={warningSB}
+          onClose={closeAllSnackbars}
+          close={closeAllSnackbars}
+          bgWhite
+        />
+      )}
+      {errorSB && (
+        <MDSnackbar
+          color="error"
+          icon="warning"
+          title="이슈 삭제"
+          content={message.message}
+          open={errorSB}
+          onClose={closeAllSnackbars}
+          close={closeAllSnackbars}
+          bgWhite
+        />
+      )}
+    </>
   );
 }
 
