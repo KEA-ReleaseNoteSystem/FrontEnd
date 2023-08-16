0b94b629da3d6fab3a69a23133cc48bbf0121322
KangDeok <rkdejr2321@naver.com>
2023-08-16 11:46:36 +0900

    [fix] 컨플릭트 해결

--- src/layouts/release/index.js
+++ src/layouts/release/index.js
@@ -23,7 +23,7 @@ import axios from 'axios';
 // Material Dashboard 2 React example components
 import DashboardLayout from "examples/LayoutContainers/DashboardLayout";
 import DashboardNavbar from "examples/Navbars/DashboardNavbar";
-import Footer from "examples/Footer";
+
 import DataTable from "examples/Tables/DataTable";
 
 import { useRecoilState } from 'recoil';
